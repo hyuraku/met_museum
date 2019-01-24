95852a8ef694c82f0e4c1bb935889d517778b89b
hyuraku <yu@hill-wood.com>
2019-01-24 23:20:21 +0900

    add params to doc

--- lib/met_museum/collection.rb
+++ lib/met_museum/collection.rb
@@ -3,12 +3,16 @@ module MetMuseum
     API_ENDPOINT = "https://collectionapi.metmuseum.org".freeze
     PUBLIC_URI = "/public/collection/v1/objects".freeze
 
+    # Return a listing of all valid Object IDs available to use
+    # @param [String] metadataDate(YYYY-MM-DD) Returns any objects with updated data after this date
     def self.objects(metadataDate = nil)
       conn = Faraday.new(:url => API_ENDPOINT)
       response = conn.get PUBLIC_URI, {:metadataDate => metadataDate}
       Oj.load(response.body)
     end
 
+    # returns a record for an object, containing all open access data about that object, including its image (if the image is available under Open Access)
+    # @param [Integer] objectID The unique Object ID for an object
     def self.object(objectID)
       response = Faraday.get "#{API_ENDPOINT}#{PUBLIC_URI}/#{objectID}"
       Oj.load(response.body)
